21e6ac975cd610a3c6f15f0d778c39c0ada4d9bd
mohit512661 <mohitrawat0171@gmail.com>
2021-08-27 15:49:28 +0530

    String to Integer(atoi)

--- strings/string_to_integer(atoi).js
+++ strings/string_to_integer(atoi).js
@@ -2,57 +2,61 @@
  * @param {string} s
  * @return {number}
  */
-var myAtoi = function (s) {
-    let isPositive = true
-    let first = true
-    let ans = 0
-    let prev = 0
-    for (let i = 0; i < s.length; i++) {
-        if (s[i] == " " || s[i] == "-" || s[i] == "+" || s[i] == "1" ||
-            s[i] == "2" || s[i] == "3" || s[i] == "4" || s[i] == "5" ||
-            s[i] == "6" || s[i] == "7" || s[i] == "8" || s[i] == "9" || s[i] == "0") {
-            if (s[i] != " ") {
-                if (s[i] == "-") {
-                    if (ans == "") {
+ var myAtoi = function(s) {
+    let isPositive = true // checking whether the final output will be +ve or -ve
+    let ans = "" // output will be stored here
+    let flag = true // marking that "-" or "+" or any digit has encountered that means 
+    // now if white space or "-" or "+" comes we will break for eg "0000-42" or "+-42" or "  -  42"
+    for(let i = 0; i < s.length; i++){
+        if(s[i] == " " || s[i] == "-" || s[i] == "+" || s[i] == "1" || s[i] == "2" || s[i] == "3" || s[i] == "4" || s[i] == "5" || s[i] == "6" || s[i] == "7" || s[i] == "8" || s[i] == "9" || s[i] == "0"){
+            if(s[i] == " "){
+                if(!flag){
+                    break
+                }
+            }
+            else{
+                if(s[i] == "-"){
+                    if(!flag){
+                        break
+                    }
+                    if(ans == ""){
                         isPositive = false
+                        flag = false
                     }
                 }
-                else if (s[i] != "+") {
-                    ans = ans * 10 + +s[i]
-                    if ((ans - +s[i]) / 10 != prev) {
-                        if (!isPositive) {
-                            return -2147483648
-                        }
-                        return 2147483648 - 1
+                else if(s[i] == "+"){
+                    if(!flag){
+                        break
                     }
-                    prev = ans
+                    if(ans == ""){
+                        isPositive = true
+                        flag = false
+                    }
+                }
+                else{
+                    ans = ans + s[i]
+                    flag = false
                 }
             }
         }
-        else {
+        else{
             break
         }
     }
-    if (ans) {
-        if (!isPositive) {
-            return -ans
+    if(ans){
+        if(!isPositive){
+            ans = -(+ans)
+            if(ans >= Math.pow(-2, 31) && ans <= Math.pow(2, 31)){
+                return ans
+            }
+            return Math.pow(-2, 31)
         }
-        return ans
+        if(+ans >= Math.pow(-2, 31) && +ans < Math.pow(2, 31)){
+            return +ans
+        }
+        let x = Math.pow(2, 31)
+        return x - 1
     }
+     
     return 0
 };
-
-
-
-
-
-
-/*
-if((ans - +s[i]) / 10 != prev){
-    if(!isPositive){
-        return -2147483648
-    }
-    return 2147483648 - 1
-}
-prev = ans
-*/
